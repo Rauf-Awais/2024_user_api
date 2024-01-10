b9d104d9015381d3cee22d18cfb6feacca0cb71f
Rauf-Awais <awais.corporate60@gmail.com>
2024-01-10 09:05:56 +0100

    Created post conflicts method

--- src/main/java/com/example/_user_api/controller/TestController.java
+++ src/main/java/com/example/_user_api/controller/TestController.java
@@ -1,2 +1,11 @@
-package com.example._user_api.controller;public class TestController {
+package com.example._user_api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class TestController {
+    @GetMapping("/test")
+    public String getTest(){
+        return "In main Branch";
+    }
+
 }
--- src/main/java/com/example/_user_api/controller/conflictsController.java
+++ src/main/java/com/example/_user_api/controller/conflictsController.java
@@ -1,4 +1,14 @@
 package com.example._user_api.controller;
 
-public class ConflictController {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class conflictsController {
+@GetMapping("/conflicts")
+    public String getConflicts(){
+    return"Getting conflicts";
+}
+
+
 }
