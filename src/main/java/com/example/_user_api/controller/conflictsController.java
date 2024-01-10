e61e48cf4ab8e736422f283ac2b8f8200b8e159e
Rauf-Awais <awais.corporate60@gmail.com>
2024-01-10 09:11:21 +0100

    Added delete conflicts method

--- src/main/java/com/example/_user_api/controller/conflictsController.java
+++ src/main/java/com/example/_user_api/controller/conflictsController.java
@@ -1,6 +1,8 @@
 package com.example._user_api.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,6 +11,14 @@ public class conflictsController {
     public String getConflicts(){
     return"Getting conflicts";
 }
+@PostMapping("/conflicts")
+    public String postConflicts(){
+    return "Posting Conflicts";
 
+}
+@DeleteMapping("/conflicts")
+    public String deleteConflicts(){
+    return " Deleting Conflicts";
+}
 
 }
